921549274630abecfb41f1dcf1cbe2a67a1c862e
Mikhail Aksenov <xeningem@gmail.com>
2018-01-31 13:41:08 +0300

    Use one JS for index.html and map.html

--- fe/map.js
+++ fe/map.js
@@ -3,77 +3,81 @@
     const XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     var coords = {latitude: 51.6754966, longitude: 39.2088823}
 
-    var lastbusquery = document.getElementById('lastbusquery')
+    const lastbusquery = document.getElementById('lastbusquery');
     var my_map
     var BusIconContentLayout
-    var info = document.getElementById('info')
-    var businfo = document.getElementById('businfo')
+    const info = document.getElementById('info');
+    const businfo = document.getElementById('businfo');
 
-    document.getElementById('lastbus').onclick = function (event) {
+    document.getElementById('lastbus').onclick = function () {
         const bus_query = lastbusquery.value
         get_bus_positions(bus_query)
     }
 
-    document.getElementById('lastbus_codd').onclick = function (event) {
-        const bus_query = lastbusquery.value
-        get_bus_codd_positions(bus_query)
-    }
 
     lastbusquery.onkeyup = function (event) {
         event.preventDefault()
         if (event.keyCode === 13) {
             const bus_query = lastbusquery.value
-            get_bus_codd_positions(bus_query)
+            if (my_map){
+                get_bus_codd_positions(bus_query)
+            }
+            else{
+                get_bus_positions(bus_query)
+            }
         }
     }
 
     if ("geolocation" in navigator) {
-        var nextbusgeo = document.getElementById('nextbusgeo')
+        const nextbusgeo = document.getElementById('nextbusgeo');
         nextbusgeo.onclick = function (event) {
             event.preventDefault()
-            get_current_pos(info)
+            get_current_pos()
         }
-        get_current_pos(info)
+        get_current_pos()
     }
 
 
-    function get_current_pos(info) {
+    function get_current_pos() {
         navigator.geolocation.getCurrentPosition(get_bus_arrival)
     }
 
     function get_bus_arrival(position) {
-        var xhr = new XHR()
+        const xhr = new XHR();
         coords = position.coords
-        var params = 'lat=' + encodeURIComponent(coords.latitude) + '&lon=' + encodeURIComponent(coords.longitude);
+        const params = 'lat=' + encodeURIComponent(coords.latitude) + '&lon=' + encodeURIComponent(coords.longitude);
         xhr.open('GET', '/arrival?' + params, true);
         xhr.send()
         xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
+            if (this.readyState !== 4) return;
 
-            if (this.status != 200) {
+            if (this.status !== 200) {
                 info.innerHTML = 'Ошибка: ' + (this.status ? this.statusText : 'запрос не удался')
                 return
             }
-            var data = JSON.parse(this.responseText)
+            const data = JSON.parse(this.responseText);
             info.innerHTML = data.text
-            // lastbusquery.value = data.routes
         }
     }
 
     function get_bus_positions(query) {
-        var xhr = new XHR()
+        const xhr = new XHR();
 
         var params = 'q=' + encodeURIComponent(query)
+        if (coords) {
+            params += '&lat=' + encodeURIComponent(coords.latitude)
+            params += '&lon=' + encodeURIComponent(coords.longitude)
+        }
         xhr.open('GET', '/businfo?' + params, true);
         xhr.send()
         xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
+            if (this.readyState !== 4) return;
 
-            if (this.status != 200) {
+            if (this.status !== 200) {
                 info.innerHTML = 'Ошибка: ' + (this.status ? this.statusText : 'запрос не удался')
                 return
             }
-            var data = JSON.parse(this.responseText)
+            const data = JSON.parse(this.responseText);
             const q = data.q
             const text = data.text
             businfo.innerHTML = 'Маршруты: ' + q + '\n' + text
@@ -81,7 +85,7 @@
     }
 
     function get_bus_codd_positions(query) {
-        var xhr = new XHR()
+        const xhr = new XHR();
 
         var params = 'q=' + encodeURIComponent(query)
         if (coords) {
@@ -91,13 +95,13 @@
         xhr.open('GET', '/coddbus?' + params, true);
         xhr.send()
         xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
+            if (this.readyState !== 4) return;
 
-            if (this.status != 200) {
+            if (this.status !== 200) {
                 info.innerHTML = 'Ошибка: ' + (this.status ? this.statusText : 'запрос не удался')
                 return
             }
-            var data = JSON.parse(this.responseText)
+            const data = JSON.parse(this.responseText);
             update_map(data.result)
         }
     }
@@ -105,10 +109,16 @@
     function update_map(buses) {
         my_map.geoObjects.removeAll()
         buses.forEach(function (bus) {
-            const hint = bus.last_time_ + '; ' + bus.azimuth
-            const desc = bus.last_time_ + JSON.stringify(bus)
-            my_map.geoObjects.add(new BusMark(bus.last_lat_, bus.last_lon_, bus.azimuth, bus.route_name_.trim(), hint, desc))
+            add_bus(bus)
         })
+    }
+
+    function add_bus(bus) {
+        const hint = bus.last_time_ + '; ' + bus.azimuth
+        const desc = bus.last_time_ + JSON.stringify(bus, null, ' ')
+        const bus_mark = new BusMark(bus.last_lat_, bus.last_lon_, bus.azimuth, bus.route_name_.trim(), hint, desc)
+        my_map.geoObjects.add(bus_mark)
+        return bus_mark
     }
 
 
@@ -132,16 +142,21 @@
             // Смещение слоя с содержимым относительно слоя с картинкой.
             iconContentOffset: [0, 0],
             // Макет содержимого.
-            iconContentLayout: BusIconContentLayout
+            iconContentLayout: BusIconContentLayout,
         });
-
         return this.placemark
     }
 
-
-    ymaps.ready(ymap_show);
+    if ('ymaps' in window) {
+        ymaps.ready(ymap_show);
+    }
 
     function ymap_show() {
+        document.getElementById('lastbus_codd').onclick = function () {
+            const bus_query = lastbusquery.value
+            get_bus_codd_positions(bus_query)
+        }
+
         my_map = new ymaps.Map('map', {
             center: [coords.latitude, coords.longitude],
             zoom: 14
@@ -150,7 +165,7 @@
         })
 
         BusIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<img class="bus-icon" style=" z-index: -1; transform: rotate({{properties.rotation}}deg); "src="arrow.png">' +
+            '<img class="bus-icon" style=" z-index: -1; transform: rotate({{properties.rotation}}deg);" src="arrow.png">' +
             '<ymaps class="bus-title" style="z-index: -2; color: orange; font-weight: bold;"> $[properties.iconContent] </ymaps>'
         )
 
@@ -160,7 +175,7 @@
     }
 
     function test_mark() {
-        var bus_obj = {
+        const bus_obj = {
             "obj_id_": 0,
             "proj_id_": 0,
             "last_speed_": 0,
@@ -174,24 +189,23 @@
             "route_name_": "18 ",
             "type_proj": 0,
             "lowfloor": 0
-        }
+        };
 
         my_map.setCenter([bus_obj.last_lat_, bus_obj.last_lon_])
         my_map.geoObjects.removeAll()
 
-        var bus_marker = new BusMark(bus_obj.last_lat_, bus_obj.last_lon_, 0, "text", "hhhhh", 'bbbbb')
+        const bus_mark = add_bus(bus_obj)
 
-        var marker = new ymaps.Placemark([bus_obj.last_lat_, bus_obj.last_lon_], {
+        const marker = new ymaps.Placemark([bus_obj.last_lat_, bus_obj.last_lon_], {
             rotation: 0
         }, {});
         my_map.geoObjects.add(marker);
-        my_map.geoObjects.add(bus_marker);
         var i = 0
         // начать повторы с интервалом 2 сек
-        var timerId = setInterval(function () {
-            var rotation = (5 * (i++))
-            bus_marker.properties.set('rotation', rotation);
-            bus_marker.properties.set('iconContent', rotation);
+        const timerId = setInterval(function () {
+            const rotation = (5 * (i++));
+            bus_mark.properties.set('rotation', rotation);
+            bus_mark.properties.set('iconContent', rotation);
             // bus_marker.balloon.open();
         }, 100);
 
